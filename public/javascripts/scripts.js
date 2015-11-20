b8740cbef14288f752e7fd4bc08ace8b77afa6e2
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-19 21:24:47 -0700

    added animation & static button

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-  $("li.page-scroll a").click(function(e) {
-    e.preventDefault();
-    scrollToAnchor($(e.target).attr('href'));
+    $("li.page-scroll a").click(function(e) {
+      e.preventDefault();
+      scrollToAnchor($(e.target).attr('href'));
+    })
+    $('.pull-down').each(function() {
+      $(this).css('margin-top', $(this).parent().height()-
+    $(this).height())
+    });
+    $('#projects').click(function (e) {
+      e.preventDefault()
+      var next = $('.active').next().length > 0 ? $('.active').next() : $('.nav li').first();
+      console.log(next)
+      scrollToAnchor($(next).attr('href'));
+    });
 })
