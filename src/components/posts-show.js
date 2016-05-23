4bdae0dc7d1e8b95cbec56621f326fad19726dda
Ian Behrmann <ianjbehrmann@gmail.com>
2016-05-23 17:04:57 -0600

    change to text for testing git

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -26,7 +26,7 @@ class PostsShow extends Component {
 
         return (
             <div>
-                <Link to="/">Back to index</Link>
+                <Link to="/">Back to Home</Link>
                 <button
                     className="btn btn-danger pull-xs-right"
                     onClick={this.onDeleteClick.bind(this)}>
